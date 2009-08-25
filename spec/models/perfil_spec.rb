620ee30cd2b041e7389145f7567e05ed318d8e58
Rodrigo Yoshima <rodrigoy@gmail.com>
2009-08-25 10:51:31 -0300

    corrigindo

--- spec/models/perfil_spec.rb
+++ spec/models/perfil_spec.rb
@@ -24,7 +24,7 @@ describe Perfil do
   end
   
   it "deve validar usuario/senha incorretos" do
-    Perfil.validar('teste1', 'senha321').should be_false
+    Perfil.validar('teste1', 'senha321').should be_nil
   end
   
   it "deve validar usuario/senha corretos" do
