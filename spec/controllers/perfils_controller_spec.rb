42e198c693c20d70cb56b85b3125ab7ea1109d29
Rodrigo Yoshima <rodrigoy@gmail.com>
2009-08-28 11:46:26 -0300

    problema no PerfilsControllerSpec ;

--- spec/controllers/perfils_controller_spec.rb
+++ spec/controllers/perfils_controller_spec.rb
@@ -1,9 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe PerfilsController do
-
+  before(:all) do
+    @perfil = Perfil.new(:id => 1, :login => 'admin', :senha => 'teste123', :nome => "Administrador")
+  end  
+  
   it "deve mostrar o usuário passado como parâmetro" do
-    controller.should_receive(:login_required).and_return(true)
+    #controller.should_receive(:login_required).and_return(true)
+    Perfil.should_receive(:find).with("1").and_return(@perfil)
     get(:controller => 'perfils', :action => 'show', :id => "1")
     response.should be_success
   end
